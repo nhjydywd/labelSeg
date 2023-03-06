4de72bacd4b72b42d71707ee94b83e5980b8bff2
670625410@qq.com <670625410@qq.com>
2023-03-06 17:11:47 +0800

    Init

---
+++ trans_ch.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>labelSeg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>上一张</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>下一张</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="263"/>
+        <source>绘制标注</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="291"/>
+        <source>擦除标注</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="310"/>
+        <source>重置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="335"/>
+        <source>自动标注</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="363"/>
+        <source>固定尺寸</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="382"/>
+        <source>保存</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="414"/>
+        <source>移除此图片</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="439"/>
+        <source>导出所有已标注</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="473"/>
+        <source>1px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="495"/>
+        <source>笔刷大小</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="505"/>
+        <location filename="mainwindow.ui" line="525"/>
+        <source>50%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="547"/>
+        <source>原图透明度</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="579"/>
+        <source>标注透明度</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="636"/>
+        <source>语言</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="646"/>
+        <source>中文</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="651"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ trans_en.ts
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>labelSeg</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>上一张</source>
+        <translation>Previous</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <source>下一张</source>
+        <translation>Next</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="263"/>
+        <source>绘制标注</source>
+        <translation>Draw</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="291"/>
+        <source>擦除标注</source>
+        <translation>Wipe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="310"/>
+        <source>重置</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="335"/>
+        <source>自动标注</source>
+        <translation>Auto Label</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="363"/>
+        <source>固定尺寸</source>
+        <translation>Fix Size</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="382"/>
+        <location filename="mainwindow.cpp" line="533"/>
+        <source>保存</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="414"/>
+        <source>移除此图片</source>
+        <translation>Remove Image</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="439"/>
+        <source>导出所有已标注</source>
+        <translation>Export All</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="473"/>
+        <source>1px</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="495"/>
+        <source>笔刷大小</source>
+        <translation>Pencil Size</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="505"/>
+        <location filename="mainwindow.ui" line="525"/>
+        <source>50%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="547"/>
+        <source>原图透明度</source>
+        <translation>Image Transparency</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="579"/>
+        <source>标注透明度</source>
+        <translation>Label Transparency</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="636"/>
+        <source>语言</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="643"/>
+        <source>项目</source>
+        <translation>Project</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="650"/>
+        <source>预标注模型</source>
+        <translation>Model for Prelabel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="660"/>
+        <source>中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="665"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="670"/>
+        <source>创建新项目</source>
+        <translation>Create New Project</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="675"/>
+        <source>打开已有项目</source>
+        <translation>Open Exsisting Project</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>无效的文件</source>
+        <translation>Invalid File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>请重新选择项目</source>
+        <translation>Please select another file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="183"/>
+        <source>文件夹非空</source>
+        <translation>Directory is not empty</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="183"/>
+        <source>请选择或创建一个空的文件夹</source>
+        <translation>Please select or create an empty directiry</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="461"/>
+        <location filename="mainwindow.cpp" line="500"/>
+        <source>完成</source>
+        <translation>Finished</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="461"/>
+        <source>已将%1移动到%2</source>
+        <translation>%1 has been moved to %2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="479"/>
+        <source>提示</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="479"/>
+        <source>会将%1张已标注的图片及标注转移到目标文件夹</source>
+        <translation>%1 annotated images and their labels will be moved to the target directory</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="480"/>
+        <source>选择导出到的文件夹</source>
+        <translation>Select an directory to export</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="500"/>
+        <source>已将%1张图片及标注导出到&apos;%2&apos;</source>
+        <translation>%1 images and their labels have been exported to %2</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="531"/>
+        <source>未保存</source>
+        <translation>Unsaved</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="532"/>
+        <source>是否保存更改？</source>
+        <translation>Save changes?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="534"/>
+        <source>放弃</source>
+        <translation>Abandon</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="556"/>
+        <source>错误</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="556"/>
+        <source>文件不存在！</source>
+        <translation>File doesn&apos;t exist!</translation>
+    </message>
+</context>
+</TS>
